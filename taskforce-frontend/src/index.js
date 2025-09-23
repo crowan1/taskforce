49131b4caa591c6ffa12ebcdd6cfd43828d5ce68
crowan1 <144345726+crowan1@users.noreply.github.com>
2025-09-23 11:40:09 +0200

    Clean: remove build folder and service worker from git tracking

--- taskforce-frontend/src/index.js
+++ taskforce-frontend/src/index.js
@@ -11,31 +11,5 @@ root.render(
   </React.StrictMode>
 );
  
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', {
-      scope: '/'
-    })
-      .then((registration) => {
-        console.log('Service Worker enregistré avec succès:', registration.scope);
-         
-        registration.addEventListener('updatefound', () => {
-          const newWorker = registration.installing;
-          newWorker.addEventListener('statechange', () => {
-            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-              console.log('Nouvelle version du Service Worker disponible');
-            }
-          });
-        });
-      })
-      .catch((registrationError) => {
-        console.warn('Échec de l\'enregistrement du Service Worker:', registrationError);
-      });
-  });
- 
-  navigator.serviceWorker.addEventListener('message', (event) => {
-    console.log('Message du Service Worker:', event.data);
-  });
-}
  
 reportWebVitals();
