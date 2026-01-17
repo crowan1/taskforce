a4cb010657b6e2f5033c8402d0d5fa8789cbbc1f
crowan1 <144345726+crowan1@users.noreply.github.com>
2026-01-17 14:55:03 +0100

    Feature : Add CI/CD pipeline

--- taskforce-frontend/src/setupTests.js
+++ taskforce-frontend/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+jest.mock('react-helmet-async', () => {
+  const React = require('react');
+  return {
+    Helmet: ({ children }) => React.createElement(React.Fragment, null, children),
+    HelmetProvider: ({ children }) => React.createElement(React.Fragment, null, children)
+  };
+});
